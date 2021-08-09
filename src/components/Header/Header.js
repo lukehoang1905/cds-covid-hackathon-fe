6e23cfd8b5a77002096f921969e099e7b6cc71ac
Vu_Hong_Son <sonvuhong177@gmail.com>
2021-08-09 16:22:37 +0700

    create NavBar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+// import { useSelector } from "react-redux";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <Navbar bg="success" expand="lg">
+      <Navbar.Brand>
+        <img src="" alt="Companylogo" width="200px" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="mr-auto my-2 my-lg-0"
+          style={{ maxHeight: "100px" }}
+          navbarScroll
+        >
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/request/form">
+            Detail
+          </Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="mr-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Header;
